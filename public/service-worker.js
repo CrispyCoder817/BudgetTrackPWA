d492612f2fe7149aa9867f08f74dc4bd30a79ffb
Brandon G.M <brandongmerritt@gmail.com>
2021-05-27 10:32:25 -0400

    Install

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const FILES_TO_CACHE = [
+"/",
+"/index.html",
+"/styles.css",
+"/index.js",
+"/public/icons/icon-192x192.png",
+"/public/icons/icon-512x512.png",
+"/manifest.json",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
